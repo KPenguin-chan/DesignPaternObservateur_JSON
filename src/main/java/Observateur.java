ac5c0bc5bbcb8b31c5411d043e4f91bd5ee26288
Julie Leroy <22301676@me.uvsq.fr>
2025-01-27 11:32:03 +0100

    ajout documentation

--- src/main/java/Observateur.java
+++ src/main/java/Observateur.java
@@ -1,3 +1,9 @@
 public interface Observateur {
+    /**
+     * La méthode actualise prend en paramètre un Objet Observable.
+     *Elle actualise la listeAbandon en réccupérant le dernier abandon de la course
+     *
+     * @param observable
+     */
     public void actualise(Observable observable);
 }
