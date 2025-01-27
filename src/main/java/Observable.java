afcb78d8a0ed6679f589b4c0d3c8c05ddf360eaf
Julie Leroy <22301676@me.uvsq.fr>
2025-01-27 11:31:12 +0100

    ajout documentation

--- src/main/java/Observable.java
+++ src/main/java/Observable.java
@@ -1,5 +1,15 @@
 public interface Observable {
+    /**
+     * Enregistre les observateurs dans la liste des Observateurs
+     * @param observateur
+     */
     public void enregistreObservateurs(Observateur observateur);
+    /** Retire un observateur de la liste des observateurs
+     * @param observateur
+     */
     public void desenregistreObservateurs(Observateur observateur);
+    /**
+     * Informe les observateur de la liste des changement de l'objet DirectionCourse
+     */
     public void informeObservateurs();
 }
